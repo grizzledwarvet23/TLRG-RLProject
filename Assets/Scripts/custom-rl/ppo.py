c56b6f3d034e058f9f93254e76a51e16398279b9
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2024-04-02 00:30:41 -0500

    succesfully established two way communication between python and unity

--- Assets/Scripts/custom-rl/ppo.py
+++ Assets/Scripts/custom-rl/ppo.py
@@ -1,10 +1,29 @@
 import numpy as np
 import torch
 import socket
+#just import pytorch:
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Categorical
+import select
+import time
+
+
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5065
-MESSAGE = "Hello, UNITY!"
+MESSAGE = "YOUR DATA"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+
+#set timeout to 5 seconds
+sock.settimeout(5)
+
+while True:
+    sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+    data, addr = sock.recvfrom(1024)
+    print(data.decode("utf-8"))
+    #do like a thing where it ends loop if not heard from unity in 5 seconds:
+    
